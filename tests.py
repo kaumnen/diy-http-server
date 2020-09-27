7087aaaff12ced3584be532d99fcb6fa603c0a1b
Aleksej Komnenovic <main@komnen.xyz>
2020-09-27 03:31:23 +0200

    minor tests.py addings

--- tests.py
+++ tests.py
@@ -1,7 +1,19 @@
 import unittest as ut
 import requests as rq
-from server import Server
+from server import Server_operations
+from config import Server_config
+
+
+http_server_config = Server_config('127.0.0.1', 5525)
+while not http_server_config.connection_established:
+    http_server_ops = http_server_config.listening()
 
 class testResponses(ut.TestCase):
     def GETtest(self):
+        
+        test = [http_server_config.client_socket]
         self.assertEqual()
+
+
+
+#still to be finished
