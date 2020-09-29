45841104a63fb83c68e0bbe457512aef59f41bf3
Aleksej Komnenovic <main@komnen.xyz>
2020-09-29 16:17:22 +0200

    completely changed organisation of server.py

--- connection.py
+++ connection.py
@@ -8,20 +8,19 @@ class Server_config:
     def __init__(self, HOST, PORT):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind((HOST, PORT))
-        self.connection_established = False
 
     #listening as long as there is not a connection
-    async def listening(self, n_of_connections = 5):
+    async def listening(self, n_of_connections = 1):
 
         #listen for connection
-        self.s.listen(n_of_connections)
+        
         
         while True:
             self.client_socket, self.address = self.s.accept()
+            self.s.listen(n_of_connections)
 
             #when connection happens, returns server object, who is initialised with client_socket object
-            if self.client_socket:
-                print(f'Connection from {self.address} has been established!')
-                self.client_socket.send('Welcome to the server!\n\nSend \'HELP\' to get list of available commands!\n\n'.encode('ascii'))
-                self.connection_established = True
-                await asyncio.create_task(Server_operations(self.client_socket).maintaining_connection())
+            
+            print(f'Connection from {self.address} has been established!')
+            await asyncio.create_task(Server_operations(self.client_socket).communication())
+            
--- server.py
+++ server.py
@@ -9,92 +9,75 @@ class Server_operations:
         self.protocol_beta = {'word':'definition'}
         self.shutdown = False
 
+    async def messenger(self, message):
+        self.client_socket.send(message.encode('utf-8'))
+
+    def decoder(self):
+        return self.client_socket.recv(1024).decode().strip()   
+
     async def communication(self):
-        
-            self.client_socket.send('>>% '.encode('utf-8'))
-            self.reply = self.client_socket.recv(1024).decode().strip() 
-
-    #GET method
-    async def get_definition(self):
-        if len(self.reply.split()) == 2:
-                try:
-                    self.client_socket.send(f'\nDEFINITION - {str(self.protocol_beta[self.reply.split()[1]])}\n\n'.encode('utf-8'))
-                except KeyError:
-                    self.client_socket.send(f'\nERROR - word \'{self.reply.split()[1]}\' is not defined!\n\n'.encode('utf-8'))
-    
-    #SET method
-    async def set_definition(self):
-        if len(self.reply.split()) < 3:
-            self.client_socket.send('Error! Type HELP for more information.\n'.encode('utf-8'))
-        else:    
-            new_combo = self.reply.split()
-            self.protocol_beta[new_combo[1]] = ' '.join(new_combo[2:])
-            
-            self.client_socket.send('\nFinishing..\n'.encode('utf-8'))
-            time.sleep(2)
-
-            self.client_socket.send('Added following definition:\n'.encode('utf-8'))
-
-            self.client_socket.send('\n#####\n'.encode('utf-8'))
-            self.client_socket.send(f'# {new_combo[1]}: {self.protocol_beta[new_combo[1]]}'.encode('utf-8'))
-            self.client_socket.send('\n#####\n'.encode('utf-8'))
-    
-    #ALL method / displaying all words and their definitions
-    async def display_definitions(self):
-        self.client_socket.send('These are all defined words:\n'.encode('utf-8'))
-        self.client_socket.send('\n#####\n# '.encode('utf-8'))
-        
-        for i in self.protocol_beta.keys():
-            self.client_socket.send(i.encode('utf-8'))
-        
-        self.client_socket.send('\n#####\n\n'.encode('utf-8'))
-
-    #CLEAR method / clears all words and their definitions
-    async def clear_definitions(self):
-        self.client_socket.send('\nClearing all definitions. Please wait...\n'.encode('utf-8'))
-        await asyncio.sleep(2)
-        self.protocol_beta.clear()
-        self.client_socket.send('All definitions are deleted now.\n\n'.encode('utf-8'))
-
-    #HELP method / displays all available commands
-    async def display_help(self):
-        self.client_socket.send('These are all commands you can use, make sure to CAPITALISE:\n'.encode('utf-8'))
-
-        self.client_socket.send('\n#####\n'.encode('utf-8'))
-        self.client_socket.send('# GET - Get definition of word\n# SET - Set a definition for new word\n# CLEAR - Clear all definitions\n# ALL - List all defitinions\n# HELP - View all available commands\n# EXIT - You guessed it :)'.encode('utf-8'))
-        self.client_socket.send('\n#####\n\n'.encode('utf-8'))
-
-    #EXIT method / terminating connection
-    async def terminate_connection(self):
-        self.client_socket.send('Sending connection closing order. Please wait...'.encode('utf-8'))
-        self.shutdown = True
-        await asyncio.sleep(2)
-        self.client_socket.close()
-
-    #maintaining connection as long as signal for terminating connection is not received
-    async def maintaining_connection(self):
+        await self.messenger('Welcome to the server!\n\nSend \'HELP\' to get list of available commands!\n\n')
         
         while not self.shutdown:
-            
-            await self.communication()
+            await self.messenger('>>% ')
+            self.reply = self.decoder() 
 
+            #GET method
             if 'GET' in self.reply:
-                await self.get_definition()
-
-            elif 'SET' in self.reply:
-                await self.set_definition()
-
-            elif self.reply == 'ALL':
-                await self.display_definitions()
-
-            elif self.reply == 'CLEAR':
-                await self.clear_definitions()
-
-            elif self.reply == 'HELP':
-                await self.display_help()
+                if len(self.reply.split()) == 2:
+                        try:
+                            await self.messenger(f'\nDEFINITION - {str(self.protocol_beta[self.reply.split()[1]])}\n\n')
+                        except KeyError:
+                            await self.messenger(f'\nERROR - word \'{self.reply.split()[1]}\' is not defined!\n\n')
             
+            #SET method
+            elif 'SET' in self.reply:
+                if len(self.reply.split()) < 3:
+                    await self.messenger('Error! Type HELP for more information.\n')
+                else:    
+                    new_combo = self.reply.split()
+                    self.protocol_beta[new_combo[1]] = ' '.join(new_combo[2:])
+                    
+                    await self.messenger('\nFinishing..\n')
+                    time.sleep(2)
+
+                    await self.messenger('Added following definition:\n')
+
+                    await self.messenger('\n#####\n')
+                    await self.messenger(f'# {new_combo[1]}: {self.protocol_beta[new_combo[1]]}')
+                    await self.messenger('\n#####\n')
+            
+            #ALL method / displaying all words and their definitions
+            elif self.reply == 'ALL':
+                await self.messenger('These are all defined words:\n')
+                await self.messenger('\n#####\n# ')
+                
+                for i in self.protocol_beta.keys():
+                    await self.messenger(i)
+                
+                await self.messenger('\n#####\n\n')
+
+            #CLEAR method / clears all words and their definitions
+            elif self.reply == 'CLEAR':
+                await self.messenger('\nClearing all definitions. Please wait...\n')
+                await asyncio.sleep(2)
+                self.protocol_beta.clear()
+                await self.messenger('All definitions are deleted now.\n\n')
+
+            #HELP method / displays all available commands
+            elif self.reply == 'HELP':
+                await self.messenger('These are all commands you can use, make sure to CAPITALISE:\n')
+
+                await self.messenger('\n#####\n')
+                await self.messenger('# GET - Get definition of word\n# SET - Set a definition for new word\n# CLEAR - Clear all definitions\n# ALL - List all defitinions\n# HELP - View all available commands\n# EXIT - You guessed it :)')
+                await self.messenger('\n#####\n\n')
+
+            #EXIT method / terminating connection
             elif self.reply == 'EXIT':
-                await self.terminate_connection()
+                await self.messenger('Sending connection closing order. Please wait...')
+                self.shutdown = True
+                await asyncio.sleep(2)
+                self.client_socket.close()
 
             elif self.shutdown:
                 await self.terminate_connection()
