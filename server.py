66cc934070d85d93d364232791efacc18c4d70d0
Aleksej Komnenovic <main@komnen.xyz>
2020-10-01 02:04:04 +0200

    general improvements on readiblity

--- server.py
+++ server.py
@@ -1,107 +1,95 @@
 import asyncio
 
+
+async def writing_to_client(sender, message):
+    sender.write(message.encode())
+    await sender.drain()
+
+
 async def handle_echo(reader, writer):
+    await writing_to_client(writer, 'Welcome to the server!\n\nSend \'HELP\' to get list of available commands!\n\n')
 
-    writer.write('Welcome to the server!\n\nSend \'HELP\' to get list of available commands!\n\n'.encode())
-    await writer.drain()
+    addr = writer.get_extra_info('peername')
+    print(f'[*] Connected to {addr}!')
 
-    protocol_beta = {'word':'definition'}
+    protocol_beta = {'word': 'definition'}
 
     while True:
-        writer.write('>>% '.encode())
-        await writer.drain()
+        await writing_to_client(writer, '>>% ')
+
         data = await reader.read(100)
         message = data.decode()
 
-        addr = writer.get_extra_info('peername')
         print(f'Received {message} from {addr}')
 
         # GET method
         if 'GET' in message:
             if len(message.split()) == 2:
                 try:
-                    writer.write(f'\nDEFINITION - {str(protocol_beta[message.split()[1]])}\n\n'.encode())
-                    await writer.drain()
+                    await writing_to_client(writer, f'\nDEFINITION - {str(protocol_beta[message.split()[1]])}\n\n')
+
                 except KeyError:
-                    writer.write(f'\nERROR - word \'{message.split()[1]}\' is not defined!\n\n'.encode())
-                    await writer.drain()
+                    await writing_to_client(writer, f'\nERROR - word \'{message.split()[1]}\' is not defined!\n\n')
 
         # SET method
         elif 'SET' in message:
             if len(message.split()) < 3:
-                writer.write('Error! Type HELP for more information.\n'.encode())
-                await writer.drain()
+                await writing_to_client(writer, 'Error! Type HELP for more information.\n')
+
             else:
                 new_combo = message.split()
                 protocol_beta[new_combo[1]] = ' '.join(new_combo[2:])
 
-                writer.write('\nFinishing..\n'.encode())
-                await writer.drain()
+                await writing_to_client(writer, '\nFinishing..\n')
                 await asyncio.sleep(2)
 
-                writer.write('Added following definition:\n'.encode())
-                await writer.drain()
-
-                writer.write('\n#####\n'.encode())
-                await writer.drain()
-                writer.write(f'# {new_combo[1]}: {protocol_beta[new_combo[1]]}'.encode())
-                await writer.drain()
-                writer.write('\n#####\n'.encode())
-                await writer.drain()
+                await writing_to_client(writer, 'Added following definition:\n')
+                await writing_to_client(writer, '\n#####\n')
+                await writing_to_client(writer, f'# {new_combo[1]}: {protocol_beta[new_combo[1]]}')
+                await writing_to_client(writer, '\n#####\n')
 
         # ALL method / displaying all words and their definitions
         elif 'ALL' in message:
-            writer.write('These are all defined words:\n'.encode())
-            await writer.drain()
-            writer.write('\n#####\n# '.encode())
-            await writer.drain()
+            await writing_to_client(writer, 'These are all defined words:\n')
+            await writing_to_client(writer, '\n#####\n# ')
 
             for i in protocol_beta.keys():
-                writer.write(i.encode())
-                await writer.drain()
-
-            writer.write('\n#####\n\n'.encode())
-            await writer.drain()
+                await writing_to_client(writer, i + '  ')
+            await writing_to_client(writer, '\n#####\n\n')
 
         # CLEAR method / clears all words and their definitions
         elif 'CLEAR' in message:
-            writer.write('\nClearing all definitions. Please wait...\n'.encode())
-            await writer.drain()
+            await writing_to_client(writer, '\nClearing all definitions. Please wait...\n')
+
             await asyncio.sleep(2)
             protocol_beta.clear()
-            writer.write('All definitions are deleted now.\n\n'.encode())
-            await writer.drain()
+            await writing_to_client(writer, 'All definitions are deleted now.\n\n')
 
         # HELP method / displays all available commands
         elif 'HELP' in message:
-            writer.write('These are all commands you can use, make sure to CAPITALISE:\n'.encode())
-            await writer.drain()
-
-            writer.write('\n#####\n'.encode())
-            await writer.drain()
-            writer.write(
-                '# GET - Get definition of word\n# SET - Set a definition for new word\n'
-                '# CLEAR - Clear all definitions\n# ALL - List all definitions\n'
-                '# HELP - View all available commands\n# EXIT - You guessed it :)'.encode()
-                                )
-            await writer.drain()
-            writer.write('\n#####\n\n'.encode())
-            await writer.drain()
+            await writing_to_client(writer, 'These are all commands you can use, make sure to CAPITALISE:\n')
+            await writing_to_client(writer, '\n#####\n')
+            await writing_to_client(writer,
+                                    '# GET - Get definition of word\n# SET - Set a definition for new word\n'
+                                    '# CLEAR - Clear all definitions\n# ALL - List all definitions\n'
+                                    '# HELP - View all available commands\n# EXIT - You guessed it :)'
+                                    )
+            await writing_to_client(writer, '\n#####\n\n')
 
         # EXIT method / terminating connection
         elif 'EXIT' in message:
-            writer.write('Sending connection closing order. Please wait...'.encode())
-            await writer.drain()
-            shutdown = True
+            await writing_to_client(writer, 'Sending connection closing order. Please wait...')
             await asyncio.sleep(2)
             writer.close()
+            print(f'[*] Disconnected from {addr}!')
             break
 
-async def main():
 
+async def main():
     server = await asyncio.start_server(handle_echo, '127.0.0.1', 8888)
 
     async with server:
         await server.serve_forever()
+
 
 asyncio.run(main())
--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class Test_responses(ut.TestCase):
 
     def setUp(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect(('127.0.0.1', 5525))
+        self.s.connect(('127.0.0.1', 8888))
 
     def tearDown(self):
         self.s.send('EXIT'.encode('utf-8'))
@@ -17,7 +17,6 @@ class Test_responses(ut.TestCase):
         self.s.send('GET word'.encode('utf-8'))
         self.s.recv(1024)
         self.assertEqual(self.s.recv(4096).decode().strip(), 'DEFINITION - definition')
-
 
 if __name__ == '__main__':
     ut.main()
